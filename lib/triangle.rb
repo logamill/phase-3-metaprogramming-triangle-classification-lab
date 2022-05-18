8b41fa2bf9a1d0025aa5a99da21b1a34f1268043
Logan Miller <loganmiller214@gmail.com>
2022-05-18 11:16:14 -0400

    lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+    
+  end
+    
+
+  def valid_triangle(a, b, c)
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a + b <= c || b + c <= a || a + c <= b
+      raise TriangleError
+    end
+  end
+
+
+  def kind 
+    valid_triangle(side_one, side_two, side_three)
+    if side_one == side_two && side_two == side_three
+      return :equilateral
+    elsif side_one == side_two || side_one == side_three || side_two == side_three
+      return :isosceles
+    elsif side_one != side_two && side_two != side_three 
+      return :scalene 
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
